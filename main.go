b97a49aa57f7e0e00c18e75c1bd7bbfed3ca5208
kuro <93798838+sp1st@users.noreply.github.com>
2022-10-25 23:31:12 +0900

    citiesAPI

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 type City struct {
@@ -17,21 +19,57 @@ type City struct {
 	Population  int    `json:"population,omitempty" db:"Population"`
 }
 
+type addedCity struct {
+	Name        string `json:"name,omitempty" db:"Name"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty" db:"Population"`
+}
+
+var (
+	db *sqlx.DB
+)
+
 func main() {
-	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to the Database: %s", err)
 	}
+	db = _db
 
-	fmt.Printf("Connected!")
+	e := echo.New()
 
-	db.Exec("INSERt INTO city (Name, CountryCode, District, Population) VALUES ('Hogsmeade', 'JPN', 'Hogsmeade', 3000)")
+	e.GET("/cities/:cityName", getCityInfoHandler)
 
-	cities := []City{}
-	// db.Select(&cities, "SELECT * FROM city WHERE CountryCode='JPN'")
+	e.POST("/cities", addCityInfoHandler)
 
-	fmt.Println("日本の都市一覧")
-	for _, city := range cities {
-		fmt.Printf("都市名: %s, 人口: %d人\n", city.Name, city.Population)
+	e.Start(":4000")
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	city := City{}
+	db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
+	if city.Name == "" {
+		return c.NoContent(http.StatusNotFound)
 	}
+
+	return c.JSON(http.StatusOK, city)
+}
+
+func addCityInfoHandler(c echo.Context) error {
+	var city addedCity
+
+	if err := c.Bind(&city); err != nil {
+		return c.JSON(http.StatusBadRequest, city)
+	}
+	fmt.Println(city)
+
+	if _, err := db.NamedExec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (:Name, :CountryCode, :District, :Population)", &city); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusOK, city)
 }
